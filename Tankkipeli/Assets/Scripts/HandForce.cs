0fdf62fa257bb505a8c4a90f1631b79aa9670380
Manninen Ossi TTK17SP <ossimanninen1@kamk.fi>
2018-06-14 10:29:54 +0300

    prototype character in prototype arena

---
+++ Tankkipeli/Assets/Scripts/HandForce.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandForce : MonoBehaviour {
+
+    public float thrust;
+    public Rigidbody rb;
+
+    public GameObject focus;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.LookAt(focus.transform);
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.Q) && this.name == "handControllerL")
+        {
+            rb.AddForce(transform.forward * thrust);
+        }
+        if (Input.GetKey(KeyCode.E) && this.name == "handControllerR")
+        {
+            rb.AddForce(transform.forward * thrust);
+        }
+        if (Input.GetKey(KeyCode.A) && this.name == "handControllerL")
+        {
+            rb.AddForce(transform.forward * thrust * -1);
+        }
+        if (Input.GetKey(KeyCode.D) && this.name == "handControllerR")
+        {
+            rb.AddForce(transform.forward * thrust * -1);
+        }
+    }
+
+}
---
+++ Tankkipeli/Assets/Scripts/PhysicMovement.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicMovement : MonoBehaviour {
+
+    Rigidbody rb;
+    public GameObject left;
+    public GameObject right;
+
+    public float speed;
+    public float turn;
+
+    float rightThrust;
+    float leftThrust;
+
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        KeyPresses();
+
+    }
+
+    void KeyPresses()
+    {
+        rightThrust = 0;
+        leftThrust = 0;
+
+        if (Input.GetKey(KeyCode.Keypad9))
+        {
+            rightThrust += 1;
+        }
+        if (Input.GetKey(KeyCode.Keypad7))
+        {
+            leftThrust += 1;
+        }
+        if (Input.GetKey(KeyCode.Keypad6))
+        {
+            rightThrust -= 1;
+        }
+        if (Input.GetKey(KeyCode.Keypad4))
+        {
+            leftThrust -= 1;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {     
+        Vector3 direction = rb.transform.position - transform.position;
+        rb.AddForceAtPosition(left.transform.forward * speed * leftThrust, left.transform.position);
+        rb.AddForceAtPosition(right.transform.forward * speed * rightThrust, right.transform.position);        
+    }
+
+   /* void Turn()
+    {
+        rb.AddTorque(transform.up * turnForce * turn);
+    }*/
+    
+}
