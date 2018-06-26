6519b23a7891d0b25274d8429b2617cdc04f354a
Niininen Touko TTK17SP <toukoniininen@kamk.fi>
2018-06-26 12:29:49 +0300

    Started to add flipper bumper.

---
+++ Assets/Scripts/FlipperBumper.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipperBumper : MonoBehaviour
+{
+    public float speed;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.attachedRigidbody.AddForce(Vector3.forward * speed);
+            Debug.Log("Trigger Entered");
+        }
+    }
+}
