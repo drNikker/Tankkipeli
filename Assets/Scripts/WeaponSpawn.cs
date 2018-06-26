cba987d191e8480b5fb3557e1776dfae3374349c
Siukola Henry TTK16SP <henrysiukola@kamk.fi>
2018-06-26 13:42:37 +0300

    weapon spawn system done

--- Assets/Scripts/WeaponSpawn.cs
+++ Assets/Scripts/WeaponSpawn.cs
@@ -7,13 +7,22 @@ public class WeaponSpawn : MonoBehaviour {
 
     public int spawnTime=10;
 
+    public bool independent;
+    public bool startWeapon;
     public bool randomWeapon;
     public bool randomPosition;
+    
 
     [Range(0, 100)] public int FlailChanceWeight;
     [Range(0, 100)] public int GreatAxeChanceWeight;
     [Range(0, 100)] public int HammerChanceWeight;
 
+    public int XPositionLowerLimit;
+    public int XPositionUpperLimit;
+    public float YPositionLowerLimit;
+    public float YPositionUpperLimit;
+    public int ZPositionLowerLimit;
+    public int ZPositionUpperLimit;
 
 
     public SPAWN_WEAPON weaponToSpawn;
@@ -27,7 +36,23 @@ public class WeaponSpawn : MonoBehaviour {
     // Use this for initialization
     void Start ()
     {
-        StartCoroutine(SpawnWeapon());
+        if (randomPosition)
+        {
+            this.gameObject.transform.position = new Vector3(Random.Range(XPositionLowerLimit, XPositionUpperLimit), Random.Range(YPositionLowerLimit, YPositionUpperLimit), Random.Range(ZPositionLowerLimit, ZPositionUpperLimit));
+        }
+
+        if (independent)
+        {
+            if (startWeapon)
+            {
+                CreateWeapon();
+            }
+            else
+            {
+                StartCoroutine(SpawnWeapon());
+            }
+        }
+
     }
 
 
@@ -35,11 +60,37 @@ public class WeaponSpawn : MonoBehaviour {
     // Update is called once per frame
     void Update ()
     {
-		
-	}
+        RaycastHit hit;
+        Ray spawnRay = new Ray(this.gameObject.transform.position, Vector3.down);
+        Debug.DrawRay(this.gameObject.transform.position, Vector3.down * 1);
+
+        if (!Physics.Raycast(spawnRay, out hit, 1) && randomPosition || hit.collider.tag != "Environment" && randomPosition)
+        {
+
+                this.gameObject.transform.position = new Vector3(Random.Range(XPositionLowerLimit, XPositionUpperLimit), Random.Range(YPositionLowerLimit, YPositionUpperLimit), Random.Range(ZPositionLowerLimit, ZPositionUpperLimit));
+            
+        }
+    }
 
     IEnumerator SpawnWeapon()
     {
+        yield return new WaitForSeconds(spawnTime);
+        CreateWeapon();
+    }
+
+
+    public enum SPAWN_WEAPON
+    {
+        FLAIL,
+        GREATAXE,
+        HAMMER,
+    }
+
+
+    public void CreateWeapon()
+    {
+
+
         if (randomWeapon)
         {
 
@@ -63,31 +114,19 @@ public class WeaponSpawn : MonoBehaviour {
             }
 
         }
-        yield return new WaitForSeconds(spawnTime);
-        CreateWeapon();
-    }
-
-
-    public enum SPAWN_WEAPON
-    {
-        FLAIL,
-        GREATAXE,
-        HAMMER,
-    }
-
-
-    public void CreateWeapon()
-    {
-
         switch (weaponToSpawn)
         {
+
+
             case SPAWN_WEAPON.GREATAXE:
                 Instantiate(GreatAxe, this.gameObject.transform.position, Quaternion.AngleAxis(90, Vector3.right));
+
 
                 break;
 
             case SPAWN_WEAPON.HAMMER:
                 Instantiate(Hammer, this.gameObject.transform.position, Quaternion.identity);
+
 
                 break;
 
@@ -96,8 +135,15 @@ public class WeaponSpawn : MonoBehaviour {
 
                 break;
         }
-        StartCoroutine(SpawnWeapon());
+        if (independent)
+        {
+            StartCoroutine(SpawnWeapon());
+        }
 
+        if (randomPosition)
+        {
+            this.gameObject.transform.position = new Vector3(Random.Range(XPositionLowerLimit, XPositionUpperLimit), Random.Range(YPositionLowerLimit, YPositionUpperLimit), Random.Range(ZPositionLowerLimit, ZPositionUpperLimit));
+        }
     }
 
 
