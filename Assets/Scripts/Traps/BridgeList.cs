a5ea82ff6426f97d18609870becdd61981a3c5f0
Niininen Touko TTK17SP <toukoniininen@kamk.fi>
2018-06-27 16:34:01 +0300

    Drawbridge trap added.

---
+++ Assets/Scripts/Traps/BridgeList.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridgeList : MonoBehaviour
+{
+    public GameObject[] bridgeList;
+
+    protected bool specialWaitTimer;
+    [Space(10)]
+    public float specialWaitTimerTime;
+    protected float originalSpecialWaitTimerTime;
+
+    void Start()
+    {
+        specialWaitTimer = true;
+    }
+
+    void Update()
+    {
+        if (specialWaitTimer == true)
+        {
+            SpecialTimer();
+        }
+    }
+
+    private void SpecialTimer()
+    {
+        specialWaitTimerTime -= Time.deltaTime;
+
+        if (specialWaitTimerTime <= 0)
+        {
+            for (int i = 0; i < bridgeList.Length; i++)
+            {
+                bridgeList[i].GetComponent<Animator>().SetBool("Open/Close", false);
+            }
+
+            specialWaitTimerTime = 0;
+            specialWaitTimer = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Traps/DrawBridge.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawBridge : MonoBehaviour
+{
+    public Animator anim;
+    //private BridgeList bridgeList;
+
+    protected bool waitTimer;
+    public float waitTimerTime;
+    protected float originalWaitTimerTime;
+
+    protected bool holdOffTimer;
+    public float holdOffTimerTime;
+    protected float originalHoldOffTimerTime;
+
+    void Start()
+    {
+        //anim.SetBool("Open/Close", true);
+        //bridgeList = GameObject.FindGameObjectWithTag("BridgeList").GetComponent<BridgeList>();
+        waitTimer = true;
+
+        originalWaitTimerTime = waitTimerTime;
+        originalHoldOffTimerTime = holdOffTimerTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (waitTimer == true)
+        {
+            WaitTimer();
+        }
+
+        if (holdOffTimer == true)
+        {
+            HoldOffTimer();
+        }
+    }
+
+    private void WaitTimer()
+    {
+        waitTimerTime -= Time.deltaTime;
+
+        if (waitTimerTime <= 0)
+        {
+            anim.SetBool("Open/Close", true);
+            waitTimerTime = originalWaitTimerTime;
+            holdOffTimer = true;
+            waitTimer = false;
+        }
+    }
+
+    private void HoldOffTimer()
+    {
+        holdOffTimerTime -= Time.deltaTime;
+
+        if (holdOffTimerTime <= 0)
+        {
+            anim.SetBool("Open/Close", false);
+            holdOffTimerTime = originalHoldOffTimerTime;
+            waitTimer = true;
+            holdOffTimer = false;
+        }
+    }
+}
