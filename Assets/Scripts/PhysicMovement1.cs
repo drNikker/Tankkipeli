2eb1c55e4f81392a00af26193aac4f6bfa159239
KAMIT\eerohau <eerohaukilahti@kamk.fi>
2018-06-27 16:54:15 +0300

    Finished PhysicsMovement (for now...). Player2.0TiredV2

---
+++ Assets/Scripts/PhysicMovement1.cs
@@ -0,0 +1,162 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicMovement1 : MonoBehaviour {
+
+    private Rigidbody rb;
+    public Vector3 com;
+
+    //wheel colliders
+    public WheelCollider leftWheelCol1;
+    public WheelCollider leftWheelCol2;
+    public WheelCollider leftWheelCol3;
+    public WheelCollider leftWheelCol4;
+    public WheelCollider rightWheelCol1;
+    public WheelCollider rightWheelCol2;
+    public WheelCollider rightWheelCol3;
+    public WheelCollider rightWheelCol4;
+
+    public float topSpeed;
+    public float accel;
+    public float brakingForce;
+    
+    float brakeTorqu;
+    
+    [SerializeField]
+    float rightTread;
+    float leftTread;
+    public float wheelDamp;
+
+    bool brakeRight = true;
+    bool brakeLeft = true;
+
+
+    public string pelaaja;
+
+	// Use this for initialization
+	void Start ()
+    {
+        brakeTorqu = brakingForce;
+        rb = GetComponent<Rigidbody>();
+        rb.centerOfMass = com;
+
+        leftWheelCol1.wheelDampingRate = wheelDamp;
+        leftWheelCol2.wheelDampingRate = wheelDamp;
+        leftWheelCol3.wheelDampingRate = wheelDamp;
+        leftWheelCol4.wheelDampingRate = wheelDamp;
+        rightWheelCol1.wheelDampingRate = wheelDamp;
+        rightWheelCol2.wheelDampingRate = wheelDamp;
+        rightWheelCol3.wheelDampingRate = wheelDamp;
+        rightWheelCol4.wheelDampingRate = wheelDamp;
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        KeyPress();
+    }
+
+    
+    void KeyPress()
+    {
+        // setup triggers as buttons
+        bool RT = Input.GetAxis(pelaaja + "TankThreadRight") > 0.0;
+        bool LT = Input.GetAxis(pelaaja + "TankThreadLeft") > 0.0;
+        
+        // Tread Speed Increases
+
+        if ((Input.GetKey(KeyCode.Keypad9) || Input.GetButton(pelaaja + "RB")) && !(Input.GetKey(KeyCode.Keypad6) || RT))
+        {
+            rightTread += accel * Time.deltaTime;
+            brakeRight = false;
+        }
+        if ((Input.GetKey(KeyCode.Keypad7) || Input.GetButton(pelaaja + "LB")) && !(Input.GetKey(KeyCode.Keypad4) || LT))
+        {
+            leftTread += accel * Time.deltaTime;
+            brakeLeft = false;
+        }
+        if ((Input.GetKey(KeyCode.Keypad6) || RT) && !(Input.GetKey(KeyCode.Keypad9) || Input.GetButton(pelaaja + "RB")))
+        {
+            rightTread -= accel * Time.deltaTime;
+            brakeRight = false;
+        }
+        if ((Input.GetKey(KeyCode.Keypad4) || LT) && !(Input.GetKey(KeyCode.Keypad7) || Input.GetButton(pelaaja + "LB")))
+        {
+            leftTread -= accel * Time.deltaTime;
+            brakeLeft = false;
+        }
+        
+        rightTread = Mathf.Clamp(rightTread,-topSpeed, topSpeed);
+        leftTread = Mathf.Clamp(leftTread, -topSpeed, topSpeed);
+
+
+        if (!(Input.GetKey(KeyCode.Keypad9) || Input.GetButton(pelaaja + "RB")) && !(Input.GetKey(KeyCode.Keypad6) || RT))
+        {
+            rightTread = 0;
+            brakeRight = true;
+
+        }
+
+        if (!(Input.GetKey(KeyCode.Keypad7) || Input.GetButton(pelaaja + "LB")) && !(Input.GetKey(KeyCode.Keypad4) || LT))
+        {
+            leftTread = 0;
+            brakeLeft = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        //Set braking
+
+        if (brakeRight)
+        {
+            rightWheelCol1.brakeTorque = brakeTorqu;
+            rightWheelCol2.brakeTorque = brakeTorqu;
+            rightWheelCol3.brakeTorque = brakeTorqu;
+            rightWheelCol4.brakeTorque = brakeTorqu;
+        }
+        else
+        {
+            rightWheelCol1.brakeTorque = 0;
+            rightWheelCol2.brakeTorque = 0;
+            rightWheelCol3.brakeTorque = 0;
+            rightWheelCol4.brakeTorque = 0;
+        }
+
+        if (brakeLeft)
+        {
+            leftWheelCol1.brakeTorque = brakeTorqu;
+            leftWheelCol2.brakeTorque = brakeTorqu;
+            leftWheelCol3.brakeTorque = brakeTorqu;
+            leftWheelCol4.brakeTorque = brakeTorqu;
+        }
+        else
+        {
+            leftWheelCol1.brakeTorque = 0;
+            leftWheelCol2.brakeTorque = 0;
+            leftWheelCol3.brakeTorque = 0;
+            leftWheelCol4.brakeTorque = 0;
+        }
+        
+        //Apply speed
+        rightWheelCol1.motorTorque = rightTread;
+        rightWheelCol2.motorTorque = rightTread;
+        leftWheelCol1.motorTorque = leftTread;
+        leftWheelCol2.motorTorque = leftTread;
+        rightWheelCol3.motorTorque = rightTread;
+        rightWheelCol4.motorTorque = rightTread;
+        leftWheelCol3.motorTorque = leftTread;
+        leftWheelCol4.motorTorque = leftTread;
+
+        
+
+    }
+    
+}
