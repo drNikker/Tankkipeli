c310fb909b5b1e5b2757adf1b9f319f88980b5c3
Niininen Touko TTK17SP <toukoniininen@kamk.fi>
2018-07-12 15:10:08 +0300

    Started to add sounds.

---
+++ Assets/Scripts/Player/PlayerAudioRandomizer.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAudioRandomizer : MonoBehaviour
+{
+    public List<AudioClip> playerAudioList = new List<AudioClip>();
+    public List<AudioClip> musicList = new List<AudioClip>();
+
+    private AudioSource audioSource;
+    private AudioClip currentAudioClip;
+    private int randomIndex;
+
+    public static PlayerAudioRandomizer Instance;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        audioSource = gameObject.GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void RandomizePlayerAudio()
+    {
+        randomIndex = Random.Range(0, playerAudioList.Count);
+
+        currentAudioClip = playerAudioList[randomIndex];
+        audioSource.clip = currentAudioClip;
+        audioSource.Play();
+    }
+
+    public void PlaySceneMusic()
+    {
+
+    }
+}
