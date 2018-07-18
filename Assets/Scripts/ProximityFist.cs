e3e912523b48ab73f884a6933cd551a00651bf79
Rarrix <32179106+Rarrix@users.noreply.github.com>
2018-07-18 10:10:16 +0300

    nascar new model

--- Assets/Scripts/ProximityFist.cs
+++ Assets/Scripts/ProximityFist.cs
@@ -105,7 +105,7 @@ public class ProximityFist : FistScript
             {
                 collision.transform.root.gameObject.GetComponent<PlayerHealth>().TakeDamage(damage);
                 finalDamageVFX = Mathf.RoundToInt(damage);
-                VFX.Emit(2 * finalDamageVFX);
+                VFX.Emit(3 * finalDamageVFX);
                 cooldown = Time.time + cooldownTime;
             }
         }
