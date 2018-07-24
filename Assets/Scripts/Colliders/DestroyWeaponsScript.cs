094fa4caf4f13e2097bf51b35fc9c47340cdab59
Niininen Touko TTK17SP <toukoniininen@kamk.fi>
2018-07-24 15:05:36 +0300

    Added colliders for destroying weapons.

---
+++ Assets/Scripts/Colliders/DestroyWeaponsScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyWeaponsScript : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.parent.tag == "Weapon")
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
