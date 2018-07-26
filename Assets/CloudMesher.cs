b1b36ced80c5d4dbd5a23b2811e27eb9dd5ac98d
EeJoKi <31925975+EeJoKi@users.noreply.github.com>
2018-07-26 15:58:18 +0300

    Falling shit progress.

---
+++ Assets/CloudMesher.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudMesher : MonoBehaviour {
+
+    public List<GameObject> startClouds;
+    public List<GameObject> cloudsToMove;
+
+    public float movementSpeed;
+
+    // Use this for initialization
+    void Start () {
+        cloudsToMove.AddRange(startClouds);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        for (int i = 0; i < cloudsToMove.Count; i++)
+        {
+            Vector3 newPos = new Vector3(cloudsToMove[i].transform.position.x + movementSpeed, cloudsToMove[i].transform.position.y, cloudsToMove[i].transform.position.z);
+            cloudsToMove[i].transform.position = newPos;
+        }
+		
+	}
+}
