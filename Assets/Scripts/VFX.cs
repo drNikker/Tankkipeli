12ea042a90c13460e81bb6bcdcbb745a6fba2cd0
Rarrix <32179106+Rarrix@users.noreply.github.com>
2018-07-30 12:42:30 +0300

    visual changes to gladiator maps

--- Assets/Scripts/VFX.cs
+++ Assets/Scripts/VFX.cs
@@ -2,13 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class VFX : MonoBehaviour {
+public class VFX : MonoBehaviour
+{
 
 
     public ParticleSystem Bumper;
+
 
     public void Bump()
     {
         Bumper.Play();
     }
+
+
 }
