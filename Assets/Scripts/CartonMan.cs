a35525148d31d4c28554aae342f41778668f8513
EeJoKi <31925975+EeJoKi@users.noreply.github.com>
2018-08-14 15:02:49 +0300

    Added CartonMan script

---
+++ Assets/Scripts/CartonMan.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CartonMan : MonoBehaviour {
+
+    private Animator anim;
+    public float animSpeedMin;
+    public float animSpeedMax;
+
+	// Use this for initialization
+	void Start () {
+        anim = gameObject.GetComponent<Animator>();
+        anim.speed = Random.Range(animSpeedMin, animSpeedMax);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
