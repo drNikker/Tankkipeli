536e355709b4138d425603bdb57c499455d8afb3
EeJoKi <31925975+EeJoKi@users.noreply.github.com>
2018-08-14 18:00:08 +0300

    Nascar scene update and made a new cartonMan prefab.

--- Assets/Scripts/CartonMan.cs
+++ Assets/Scripts/CartonMan.cs
@@ -4,14 +4,28 @@ using UnityEngine;
 
 public class CartonMan : MonoBehaviour {
 
+    public List<Material> colorMats;
+
     private Animator anim;
     public float animSpeedMin;
     public float animSpeedMax;
 
 	// Use this for initialization
 	void Start () {
+
+        //Randomize the material
+        var myRend = gameObject.transform.Find("CartonManMesh").GetComponent<SkinnedMeshRenderer>();
+        Material[] tempMats = myRend.materials;
+
+        int rndNo1 = Random.Range(0, colorMats.Count);
+        tempMats[0] = colorMats[rndNo1];
+
+        myRend.materials = tempMats;
+
+        //Get the animator
         anim = gameObject.GetComponent<Animator>();
         anim.speed = Random.Range(animSpeedMin, animSpeedMax);
+
 	}
 	
 	// Update is called once per frame
