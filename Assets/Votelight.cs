e3b96298b0abd063d425bcde2425062bf86c59e4
Rarrix <rarrix1@gmail.com>
2018-08-15 19:17:09 +0300

    Lobby changes

---
+++ Assets/Votelight.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Votelight : MonoBehaviour {
+
+
+
+    public Animator VoteLight;
+    public float count = 0;
+    public Collider Box;
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Spine01")
+        {
+           
+            count++;
+
+        }
+        
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.name == "Spine01")
+        {
+            count--;
+        }
+    }
+
+
+    void Update ()
+    {
+        VoteLight.SetFloat("Blend", count);
+	}
+}
