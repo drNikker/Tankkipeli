ed7e0b7a5b61ec729a17e78b7a4e5b48ac446212
EeJoKi <31925975+EeJoKi@users.noreply.github.com>
2018-08-16 17:09:59 +0300

    Swamp face syncing try

---
+++ Assets/SwampFace.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwampFace : MonoBehaviour {
+
+    private Animator myAnim;
+    public Animator bridgeAni;
+
+	// Use this for initialization
+	void Start () {
+        myAnim = gameObject.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(bridgeAni.GetBool("Open/Close") == true)
+        {
+            myAnim.SetBool("openEyes", true);
+            myAnim.SetBool("openEyes", false);
+        }
+	}
+}
