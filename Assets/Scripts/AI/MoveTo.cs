629d8191bb160cc5cdc5b19f8eddb0672999f509
henry Siukola <henry.siukola@hotmail.com>
2018-09-11 01:29:19 +0300

    Base for AI added

---
+++ Assets/Scripts/AI/MoveTo.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.AI;
+
+public class MoveTo : MonoBehaviour
+{
+
+    public Transform goal;
+    public GameObject[] weapons;
+    public Vector3 test;
+    NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        StartCoroutine(LookForWeapon());
+    }
+    private void Update()
+    {
+        test = agent.velocity;
+    }
+
+    IEnumerator LookForWeapon()
+    {
+        yield return new WaitForSeconds(0.2f);
+        weapons = GameObject.FindGameObjectsWithTag("Weapon");
+        goal = weapons[1].transform;
+        agent.destination = goal.position;
+    }
+}
